9b05d76beb173450a4009a5ac59a49f4c516a3e4
Abrar-Fiaz <abrarfiaz10@gmail.com>
2025-11-17 11:34:26 +0600

    4th

--- Mid/Lab task 2/task2.php
+++ Mid/Lab task 2/task2.php
@@ -5,8 +5,13 @@
     <title>Participent Regristration</title>
 </head>
 <body>
-    <h1>Participant Regristration </h2>
+    <h1>Participant Regristration </h1>
+    
     <p> Full Name</p>
+    <input type="text">
+    <p>Email</p>
+    <input type="email">
+    <p>Phone number</p>
     <input type="text">
 
     

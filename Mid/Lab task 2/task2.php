ef360027d643925954d6cc1f2a47aa934c869c56
Abrar-Fiaz <abrarfiaz10@gmail.com>
2025-11-17 11:21:35 +0600

    2nd

---
+++ Mid/Lab task 2/task2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Participent Regristration</title>
+</head>
+<body>
+    <h1>Participant Regristration </h2>
+    <p> Full Name</p>
+    <input type="text">
+</body>
+</html>
